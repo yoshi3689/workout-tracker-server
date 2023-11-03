0f7d1a04b073e66fa404e44d608294ceaa7c1406
Yoshiyuki Nagai <ynagai@my.bcit.ca>
2023-11-02 17:16:13 -0700

    create routes for user login/signup

--- app/controllers/user.controller.ts
+++ app/controllers/user.controller.ts
@@ -3,7 +3,7 @@ import { getErrorMessage } from '../utils/errors.util';
 import * as userServices from '../services/user.service';
 import { CustomRequest } from '../middlewares/verifyToken';
 
-export const loginOne = async (req: Request, res: Response) => {
+export const login = async (req: Request, res: Response) => {
   let foundUser = null;
   try {
     foundUser = await userServices.login(req.body);
@@ -14,7 +14,7 @@ export const loginOne = async (req: Request, res: Response) => {
   }
 };
 
-export const registerOne = async (req: Request, res: Response) => {
+export const register = async (req: Request, res: Response) => {
   try {
     await userServices.register(req.body);
     res.status(200).send("Inserted successfully");
--- app/services/user.service.ts
+++ app/services/user.service.ts
@@ -1,9 +1,22 @@
-import { SchemaDefinition } from "mongoose"
 import { User, IUser } from "../models/user.model"
+import { compareSync } from "bcrypt";
+import { sign } from "jsonwebtoken";
+import { SECRET } from "../middlewares/verifyToken";
 
-export const register = async (user: SchemaDefinition<IUser>): Promise<Boolean> => {
+export interface SignedInUser {
+  user: IUser;
+  token: string;
+}
+
+export const register = async (user: IUser): Promise<Boolean> => {
   try {
-    const res = await User.create(user);
+    const now = new Date();
+    const res = await User.create({
+      ...user,
+      createdAt: now,
+      lastActiveAt: now,
+      routines: []
+    });
     console.log(res.username + " is created");
     return res != null;
   } catch (err) {
@@ -11,12 +24,32 @@ export const register = async (user: SchemaDefinition<IUser>): Promise<Boolean> 
   }
 }
 
-export const login = async (user: SchemaDefinition<IUser>): Promise<IUser|null> => {
+export const login = async (user: IUser): Promise<SignedInUser> => {
   try {
-    const res = await User.findOne({ name: user.username, password: user.password });
-    res != null && console.log(res.username + " is found");
-    return res;
+    const res = await User.findOne({ name: user.username });
+    
+    if (!res) {
+      throw new Error("Name of user is not correct");
+    }
+    console.log(res.username + " is found");
+
+    const isMatch = compareSync(user.password, res.password);
+    if (!isMatch) {
+      throw new Error("password incorrect");
+    }
+
+    // TODO: update lastActiveAt to the current datetime
+
+    const token = sign(
+      { _id: res._id?.toString(), name: res.username },
+      SECRET,
+      {
+        expiresIn: "2 days",
+      }
+    );
+    res.password = "";
+    return { user: res, token };
   } catch (err) {
     throw err;
   }
-}
+};
